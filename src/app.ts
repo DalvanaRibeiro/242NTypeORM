307e565d69bbd5d0c59b5121954e29dbd02fbd8d
Dalvana Lopes Ribeiro <93749040+DalvanaRibeiro@users.noreply.github.com>
2026-03-02 19:54:01 -0300

    Integrate Swagger UI for API documentation
    Added Swagger UI integration for API documentation.

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import { usuarioRoutes } from "./routes/usuario.routes";
 
+import swaggerUi from "swagger-ui-express"
+import { swaggerSpec } from "./docs/swagger";
+
 /**
  * app.ts = configuração do Express (middlewares e rotas)
- * Sem CORS: a API só vai aceitar requisições "tranquilas" do mesmo domínio/origem.
+ 
  * (Postman/Insomnia funciona normal.)
  */
 export const app = express();
 
 // permite JSON no body
 app.use(express.json());
+//Swagger
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 // rotas
 app.use("/api", usuarioRoutes);
 
 // rota simples pra teste
 app.get("/", (req, res) => res.send("API TypeORM rodando ✅"));
+
